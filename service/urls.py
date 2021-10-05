de66994ccf830d0aaea9f6ec58faab17c43e195b
wlgp3509 <wlgp3509@naver.com>
2021-10-05 18:52:54 +0900

    10.05 1- adhd1,2,3 수정 및 어플리케이션 연동 (service 앱-> adhd_test -> adhd1,2,3)
    adhd1.html에서 extends 수정 필요

--- service/urls.py
+++ service/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('adhd_test/', views.adhd_test, name='adhd_test'),
     path('mulpsy_test/', views.mulpsy_test, name='mulpsy_test'),
     path('artpsy_test/', views.artpsy_test, name='artpsy_test'),
+    # adhd 테스트 추가-최졔 21.10.05
+    path('adhd_test/adhd1/', views.adhd1, name='adhd1'),
+    path('adhd_test/adhd2/', views.adhd2, name='adhd2'),
+    path('adhd_test/adhd3/', views.adhd3, name='adhd3'),
+
 ]
--- service/views.py
+++ service/views.py
@@ -5,6 +5,15 @@ from django.shortcuts import render
 def adhd_test(request):
     return render(request, 'adhd_test.html')
 
+# 21.10.05 adhd 1,2,3 세부 검사 페이지 추가
+def adhd1(request):
+    return render(request, 'adhd1.html')
+def adhd2(request):
+    return render(request, 'adhd2.html')
+def adhd3(request):
+    return render(request, 'adhd3.html')
+
+
 def mulpsy_test(request):
     return render(request, 'mulpsy_test.html')
 
