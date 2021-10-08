6b0ced03950a379ce55da2619f9a6485bed71ad0
wlgp3509 <wlgp3509@naver.com>
2021-10-08 19:08:39 +0900

    10.08 adhd ->app 연결 오류 잡기 성공 디자인 수정: 데이터 입력란 선택 -> 보기3(체크박스) adhd 초/중고등/양육-> 소스코드 옮기기

--- service/views.py
+++ service/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 
 #******************** 21.07.26 서비스 소개
 def adhd_test(request):
-    return render(request, 'adhd_test.html')
+    return render(request, 'ADHD/adhd_test.html')
 
 # 21.10.05 adhd 1,2,3 세부 검사 페이지 추가
-def adhd1(request):
-    return render(request, 'adhd1.html')
-def adhd2(request):
-    return render(request, 'adhd2.html')
-def adhd3(request):
-    return render(request, 'adhd3.html')
+def adhd_elementary(request):
+    return render(request, 'ADHD/adhd_elementary.html')
+def adhd_student(request):
+    return render(request, 'ADHD/adhd_student.html')
+def adhd_rearer(request):
+    return render(request, 'ADHD/adhd_rearer.html')
 
 
 def mulpsy_test(request):
