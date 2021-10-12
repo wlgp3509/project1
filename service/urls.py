c202d449fda4a44c4ae632e8a506b4774f54c487
wlgp3509 <wlgp3509@naver.com>
2021-10-12 17:58:14 +0900

    10.12 - input 보완 완료 - 초,중등 택1로 수정, 양육자박스 연동 - 우울증, 다중심리 html 연결( urls, views파일 수정 및 templates단 폴더 추가)

--- service/urls.py
+++ service/urls.py
@@ -5,13 +5,21 @@ app_name = 'service'
 
 urlpatterns = [
     # 서비스 소개 21.07.26
-
+    # 우울증, 다중지능 테스트 추가- 최졔 21.10.12
     path('adhd_test/', views.adhd_test, name='adhd_test'),
-    path('mulpsy_test/', views.mulpsy_test, name='mulpsy_test'),
-    path('artpsy_test/', views.artpsy_test, name='artpsy_test'),
-    # adhd 테스트 추가-최졔 21.10.05
+    path('multiiq_test/', views.multiiq_test, name='multiiq_test'),
+    path('depression_test/', views.depression_test, name='depression_test'),
+    # adhd 초/중등/양육자 추가_최졔 21.10.05
     path('adhd_elementary/', views.adhd_elementary, name='adhd_elementary'),
     path('adhd_student/', views.adhd_student, name='adhd_student'),
     path('adhd_rearer/', views.adhd_rearer, name='adhd_rearer'),
+
+    # 우울증 청소년/양육자 추가_최졔 21.10.12
+    path('dep_adolescent/', views.dep_adolescent, name='dep_adolescent'),
+    path('dep_rearer/', views.dep_rearer, name='dep_rearer'),
+
+    # 다중지능 청소년/양육자 추가_최졔 21.10.12
+    path('mul_adolescent/', views.mul_adolescent, name='mul_adolescent'),
+    path('mul_rearer/', views.mul_rearer, name='mul_rearer'),
 
 ]
--- service/views.py
+++ service/views.py
@@ -2,10 +2,10 @@
 from django.shortcuts import render
 
 #******************** 21.07.26 서비스 소개
+# 21.10.05 adhd 서비스 추가
 def adhd_test(request):
     return render(request, 'ADHD/adhd_test.html')
-
-# 21.10.05 adhd 1,2,3 세부 검사 페이지 추가
+# 21.10.05 adhd 초/중고/양육자 추가
 def adhd_elementary(request):
     return render(request, 'ADHD/adhd_elementary.html')
 def adhd_student(request):
@@ -13,10 +13,21 @@ def adhd_student(request):
 def adhd_rearer(request):
     return render(request, 'ADHD/adhd_rearer.html')
 
+# 21.10.12 우울증 (청소년, 양육자) 서비스 추가
+def depression_test(request):
+    return render(request, 'Depression/depression_test.html')
+def dep_adolescent(request):
+    return render(request, 'Depression/dep_adolescent.html')
+def dep_rearer(request):
+    return render(request, 'Depression/dep_rearer.html')
 
-def mulpsy_test(request):
-    return render(request, 'mulpsy_test.html')
+# 21.10.12 다중지능 (청소년, 양육자) 서비스 추가
+def multiiq_test(request):
+    return render(request, 'Multi_Iq/multiiq_test.html')
 
-def artpsy_test(request):
-    return render(request, 'artpsy_test.html')
+def mul_adolescent(request):
+    return render(request, 'Multi_Iq/mul_adolescent.html')
+def mul_rearer(request):
+    return render(request, 'Multi_Iq/mul_rearer.html')
+
 
