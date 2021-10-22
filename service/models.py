41fbe8f6a92c70b760f02954ae074be424e03b7e
wlgp3509 <wlgp3509@naver.com>
2021-10-22 18:40:28 +0900

    10.22 - database 연동 models,forms,views .py 수정 알고리즘 답변 -> 제출한 데이터 저장 - (청소년) 다중지능 html 수정(20문항까지) - 정답 0~4 -> 1~5로 수정필요

--- service/models.py
+++ service/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
+from django.forms.widgets import RadioSelect
+
 
 # 21.10.21 선택 결과 테이블 정의_최졔
 # 120p 참조
 
 class Choice(models.Model):
-    choice = models.IntegerField(default=0)
+    ADHD_CHOICES = [('0', '0'), ('1', '1'), ('2', '2'), ('3', '3')]
 
-    def __str__(self):
-        return self.choice
